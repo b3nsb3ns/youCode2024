fc99e4fd7a8bae748add08ad3fd0866c1907fb27
v4h3r <v4h3r@ugrad.cs.ubc.ca>
2024-04-06 20:59:59 -0700

    ...

--- frontend/add-venture/src/App.js
+++ frontend/add-venture/src/App.js
@@ -10,11 +10,11 @@ function App() {
   return (
       <div className="App">
         <header className="App-header">
-          <h1>LEGO Showcase</h1>
+          <h1>Adventure App</h1>
           <AddVenture
-              name="Botanical Collection - Flowers"
-              description="Create a beautiful flower display that will stay fresh all year round with this LEGO Botanical Collection."
-              imageUrl="https://www.lego.com/cdn/cs/set/assets/blteb6d782e63fd9de2/10280_Block_Standard_3.jpg?fit=crop&format=jpg&quality=80&width=635&height=440&dpr=1"
+              name="Challenge of the week:"
+              description="5-10km run, up to 1h length"
+              imageUrl="https://pbs.twimg.com/media/BUIghUqCQAEqMsa.jpg:large"
           />
           <ActivityList
 
